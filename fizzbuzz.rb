9fd64aba1c935eb090a061c2edf50b952041fe19
John Exarhouleas <exarhouleasjohn@gmail.com>
2020-08-25 04:43:57 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,17 +1,8 @@
 # Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
-
-num = 0 
+# See README.md for instructions on how to do thisnum = 0 
 
 def fizzbuzz(num)
-  if num % 3 = 0
-    num = 3
-  elseif num % 5 = 0
-    num = 5
-  elseif num % 15 = 0
-    num = 15
-  else 
-    num = 4
+  
 end
 
 
