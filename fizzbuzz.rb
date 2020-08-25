5a452969dadbb06c256c67e364e635a4d1152c9a
John Exarhouleas <exarhouleasjohn@gmail.com>
2020-08-25 04:53:39 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,15 @@
 # See README.md for instructions on how to do thisnum = 0 
 
 def fizzbuzz(num)
-  
+  if num % 3 == 0
+    num = 3
+  elseif num % 5 == 0
+    num = 5
+  elseif num % 15 == 0
+    num = 15
+  else 
+    num = 4
+  end
   
 end
 
